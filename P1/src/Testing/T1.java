89e0f0efe35596c2fef2ac8e256c1ec521fb5ac6
ebby <ebby@DESKTOP-629CAOF>
2019-02-25 12:32:24 -0500

    p1 project

---
+++ P1/src/Testing/T1.java
@@ -0,0 +1,22 @@
+package Testing;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class T1 {
+	@Test
+	public void m1() {
+		System.out.println("m1");
+
+	}@Test
+	public void m2() {
+		System.out.println("m2");
+
+}@Test
+	public void m3() {
+	System.out.println("m3");
+}@Test
+	public void m33() {
+	System.out.println("m33");
+}
+}
---
+++ P1/src/Testing2/T2.java
@@ -0,0 +1,19 @@
+package Testing2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class T2 {
+	@Test
+	public void m4() {
+		System.out.println("m4");
+
+	}@Test
+	public void m5() {
+		System.out.println("m5");
+
+}@Test
+	public void m6() {
+	System.out.println("m6");
+}
+}
